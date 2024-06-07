b9e717c477b76cbbb40a7eede4b9f1094e363d14
Christopher C Fleming <63308959+ChristopherCFleming@users.noreply.github.com>
2024-06-07 03:10:03 -0400

    Add JUnit testing for the rest of the CRUD methods

--- untitled1/src/main/java/learn/data/OrbiterFileRepository.java
+++ untitled1/src/main/java/learn/data/OrbiterFileRepository.java
@@ -1,8 +1,6 @@
 package learn.data;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +14,7 @@ public class OrbiterFileRepository {
         this.filePath = filePath;
     }
 
-    public List<Orbiter> findAll() {
+    public List<Orbiter> findAll() throws DataAccessException {
         ArrayList<Orbiter> result = new ArrayList<>();
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
         reader.readLine(); //throws away the first line, the header. If you look in the .csv, the first line tells it what each value is.
@@ -31,13 +29,15 @@ public class OrbiterFileRepository {
                     result.add(orbiter);
                 }
             }
-        }catch(IOException ex) {
-            //do Nothing for now, because if there's no file, then no big deal.
+        }catch (FileNotFoundException ex) {
+            //okay to ignore.
+        }catch (IOException ex) {
+            throw new DataAccessException(ex.getMessage(), ex);
         }
         return result;
     }
 
-    public Orbiter findByID(int orbiterID) {
+    public Orbiter findByID(int orbiterID) throws DataAccessException {
         for (Orbiter orbiter: findAll()) {//here, findAll() returns an ArrayList of orbiter objects!
             if (orbiter.getOrbiterID() == orbiterID){
                 return orbiter;
@@ -46,7 +46,7 @@ public class OrbiterFileRepository {
         return null;
     }
 
-    public List<Orbiter> findByType(OrbiterType type){
+    public List<Orbiter> findByType(OrbiterType type) throws DataAccessException {
         ArrayList<Orbiter> result = new ArrayList<>();
         for (Orbiter orbiter : findAll()) {
             if (orbiter.getType() == type) {
@@ -56,4 +56,60 @@ public class OrbiterFileRepository {
         return result;
     }
 
+    public Orbiter add(Orbiter orbiter) throws DataAccessException {
+        List<Orbiter> all = findAll();
+        int nextId = 0;
+        for (Orbiter o : all) {
+            nextId = Math.max(nextId, o.getOrbiterID());
+        }
+        nextId++; //Need to add one because orbiter ids start at 1, not 0.
+        orbiter.setOrbiterID(nextId);
+        all.add(orbiter);
+        writeAll(all);
+        return orbiter;
+    }
+
+    public boolean update(Orbiter orbiter) throws DataAccessException {
+        List<Orbiter> all = findAll();
+
+        for (int i = 0; i < all.size(); i++) {
+            if (all.get(i).getOrbiterID() == orbiter.getOrbiterID()){
+                all.set(i, orbiter);
+                writeAll(all);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteById(int orbiterId) throws DataAccessException {
+        List<Orbiter> all = findAll();
+        for (int i = 0; i < all.size(); i++) {
+            if (all.get(i).getOrbiterID() == orbiterId){
+                all.remove(i);
+                writeAll(all);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void writeAll(List<Orbiter> orbiters) throws DataAccessException {
+        try(PrintWriter writer = new PrintWriter(filePath)){
+            writer.println("orbiterId,name,type,sponsor");
+            for (Orbiter o : orbiters) {
+                writer.println(serialize(o));
+            }
+        } catch(IOException ex){
+            throw new DataAccessException(ex.getMessage(), ex);
+        }
+    }
+
+    private String serialize(Orbiter orbiter) {
+        return String.format("%s,%s,%s,%s",
+                orbiter.getOrbiterID(),
+                orbiter.getName(),
+                orbiter.getType(),
+                orbiter.getSponsor());
+    }
 }//end of class
--- untitled1/src/test/java/learn/data/OrbiterFileRepositoryTest.java
+++ untitled1/src/test/java/learn/data/OrbiterFileRepositoryTest.java
@@ -2,37 +2,54 @@ package learn.data;
 
 import learn.models.Orbiter;
 import learn.models.OrbiterType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class OrbiterFileRepositoryTest {
-    private OrbiterFileRepository repository = new OrbiterFileRepository("./data/orbiters.csv");
+
+    private static final String SEED_PATH = "./data/orbiters-seed.csv";
+    private static final String TEST_PATH = "./data/orbiters-test.csv";
+    private OrbiterFileRepository repository = new OrbiterFileRepository(TEST_PATH);
+    
+    @BeforeEach
+    void setup() throws IOException {
+        Object standardCopyOption;
+        Files.copy(Paths.get(SEED_PATH),
+                Paths.get(TEST_PATH),
+                StandardCopyOption.REPLACE_EXISTING);
+    }
 
     @Test
-    void shouldFindFiveOrbiters() {
+    void shouldFindFiveOrbiters() throws DataAccessException {
         List<Orbiter> actual = repository.findAll();
         assertNotNull(actual);
         assertEquals(5, actual.size());
     }
 
     @Test
-    void shouldFindExistingOrbiter(){
+    void shouldFindExistingOrbiter() throws DataAccessException {
         Orbiter A1 = repository.findByID(1);
         assertNotNull(A1);
         assertEquals("A1", A1.getName());
     }
 
     @Test
-    void shouldNotFindExistingOrbiter(){
+    void shouldNotFindExistingOrbiter() throws DataAccessException {
         Orbiter nope = repository.findByID(1000);
         assertNull(nope);
     }
 
     @Test
-    void shouldFindOneOfEachType(){
+    void shouldFindOneOfEachType() throws DataAccessException {
         List<Orbiter> modules = repository.findByType(OrbiterType.MODULE);
         assertNotNull(modules);
         assertEquals(1, modules.size());
@@ -54,4 +71,57 @@ class OrbiterFileRepositoryTest {
         assertEquals(1, modules.size());
     }
 
+    @Test
+    void shouldAddOrbiter() throws DataAccessException {
+        Orbiter orbiter = new Orbiter();
+        orbiter.setType(OrbiterType.MODULE);
+        orbiter.setName("Test Module");
+        orbiter.setSponsor("Test Sponsor");
+
+        Orbiter actual = repository.add(orbiter);
+
+        assertNotNull(actual);
+        assertEquals(6, actual.getOrbiterID());
+
+    }
+
+    @Test
+    void shouldUpdateExisting() throws DataAccessException {
+        Orbiter orbiter = new Orbiter();
+        orbiter.setOrbiterID(3);
+        orbiter.setName("Test Shuttle");
+        orbiter.setType(OrbiterType.SHUTTLE);
+        orbiter.setSponsor("Test Sponsor");
+
+        boolean actual = repository.update(orbiter);
+        assertTrue(actual);
+
+        Orbiter anotherActual = repository.findByID(3);
+        assertNotNull(anotherActual);
+        assertEquals("Test Shuttle", anotherActual.getName());
+        assertEquals("Test Sponsor", anotherActual.getSponsor());
+    }
+
+    @Test
+    void shouldNotUpdateExisting() throws DataAccessException {
+        Orbiter orbiter = new Orbiter();
+        orbiter.setOrbiterID(100000);
+        boolean actual = repository.update(orbiter);
+        assertFalse(actual);
+    }
+
+    @Test
+    void shouldDeleteExisting() throws DataAccessException {
+        boolean actual = repository.deleteById(2);
+        assertTrue(actual);
+
+        Orbiter o = repository.findByID(2);
+        assertNull(o);
+    }
+
+    @Test
+    void shouldNotDeleteMissing() throws DataAccessException {
+        boolean actual = repository.deleteById(200000);
+        assertFalse(actual);
+    }
 }
